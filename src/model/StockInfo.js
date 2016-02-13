46d735c1a3e20f87ce82fd13fbe19abeb3fe7ba4
Franky Lau <killalau1988@hotmail.com>
2016-02-13 14:47:17 +0800

    Index Stock Basic Info

--- src/model/StockInfo.js
+++ src/model/StockInfo.js
@@ -4,12 +4,13 @@ import mongoose from 'mongoose';
 
 let StockInfoSchema = new mongoose.Schema({
     location: { type: String, index: true },
-    code: { type: String, unique: true },
+    code: { type: String, index: true },
     name: String,
     stdName: String,
 });
 
-// StockInfoSchema.set('autoIndex', false);
+StockInfoSchema.index({location:1, code:1}, {unique: true});
+StockInfoSchema.set('autoIndex', false);
 
 let StockInfo = mongoose.model('StockInfo', StockInfoSchema);
 
