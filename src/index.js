1d8efe7a821f5a1a5623760052f8ce8c581c225b
Franky Lau <killalau1988@hotmail.com>
2016-03-17 22:46:32 +0800

    Change npm action

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 require("babel-core/register");
 
 var crawler = require('./crawler/app.js');
-crawler.crawl();
+
+var argv = process.argv.slice();
+var nodepath = argv.shift();
+var indexfile = argv.shift();
+
+switch (argv[0]) {
+    case 'crawl':
+        var location = argv[1] || 'HK';
+        crawler.crawl(location);       
+        break;
+
+    default:
+        break;
+}
