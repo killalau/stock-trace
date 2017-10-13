1639bb894518bb5f7f9ce250f982abb6f41a293c
Franky Lau <franky.lau@play-asia.com>
2017-10-13 17:44:37 +0800

    feat: fetch the hk stock list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-require("babel-core/register");
-
 var crawler = require('./crawler/app.js');
 var server = require('./server/app.js');
 
@@ -8,15 +6,15 @@ var nodepath = argv.shift();
 var indexfile = argv.shift();
 
 switch (argv[0]) {
-    case 'crawl':
-        var location = argv[1] || 'HK';
-        crawler.crawl(location);       
-        break;
+  case 'crawl':
+    var location = argv[1] || 'HK';
+    crawler.crawl(location);
+    break;
 
-    case 'server':
-        var port = argv[1] || 3000;
-        server.start(port);
-        break;
-    default:
-        break;
+  case 'server':
+    var port = argv[1] || 3000;
+    server.start(port);
+    break;
+  default:
+    break;
 }
